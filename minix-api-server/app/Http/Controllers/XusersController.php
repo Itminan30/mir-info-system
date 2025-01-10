3c275e52c4fbae4c0f03f15330405312af0a588b
Itminan30 <itminan30@gmail.com>
2025-01-10 12:23:21 +0600

    api for creating new x users has beed created and tested with postman

--- minix-api-server/app/Http/Controllers/XusersController.php
+++ minix-api-server/app/Http/Controllers/XusersController.php
@@ -6,6 +6,8 @@ use App\Models\Xusers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreXusersRequest;
 use App\Http\Requests\UpdateXusersRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class XusersController extends Controller
 {
@@ -15,14 +17,34 @@ class XusersController extends Controller
     public function index()
     {
         //
+        return Xusers::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreXusersRequest $request)
+    public function store(Request $request)
     {
-        //
+        $fields = $request->validate([
+            'user_name' => 'required|string|max:255',
+            'twitter_handle' => 'required|string|max:255|unique:xusers',
+            'email' => 'required|email|unique:xusers',
+            'password' => 'required|string|min:8'
+        ]);
+
+        $hashed_password = Hash::make($request->password);
+
+        $newUser = Xusers::create([
+            'user_name' => $request->user_name,
+            'twitter_handle' => $request->twitter_handle,
+            'email' => $request->email,
+            'password' => $hashed_password
+        ]);
+
+        return response()->json([
+            'message' => 'User Created Successfully',
+            'user' => $newUser
+        ], 201);
     }
 
     /**
@@ -36,7 +58,7 @@ class XusersController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateXusersRequest $request, Xusers $xusers)
+    public function update(Request $request, Xusers $xusers)
     {
         //
     }
--- minix-api-server/routes/api.php
+++ minix-api-server/routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\XusersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+
+Route::apiResource("xusers", XusersController::class);
 
 Route::get("/", function() {
     return "API Created";
