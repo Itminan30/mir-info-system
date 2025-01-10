f36cecd82cd8878c599a7fff0b79547e3796308e
Itminan30 <itminan30@gmail.com>
2025-01-11 01:25:09 +0600

    frontend scaffolded with vite, dependencies installed are tailwind, daisyui, react-router

---
+++ minix-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-3xl text-red-500 font-bold'>
+        Tailwind working
+      </p>
+    </>
+  )
+}
+
+export default App
