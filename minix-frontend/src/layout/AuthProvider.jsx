d4e24d816a11449e53b013c8a3c41ff935a5f0d8
Itminan30 <itminan30@gmail.com>
2025-01-12 01:04:04 +0600

    login system implemented

--- minix-frontend/src/layout/AuthProvider.jsx
+++ minix-frontend/src/layout/AuthProvider.jsx
@@ -1,15 +1,15 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
-/* eslint-disable no-undef */
 import { createContext, useContext, useState } from "react";
 
-const AuthContext = createContext({children});
-export const AuthProvider = () => {
+const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const login = (userData) => setUser(userData);
     const logout = () => setUser(null);
 
     return (
-        <AuthContext.Provider value={{user, login, logout}}>
+        <AuthContext.Provider value={{ user, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
--- minix-frontend/src/pages/Login/Login.jsx
+++ minix-frontend/src/pages/Login/Login.jsx
@@ -1,11 +1,77 @@
+import { Link, useNavigate } from "react-router";
+import authImg from "../../assets/authenticationImg.png";
+import { useState } from "react";
+import { useAuth } from "../../layout/AuthProvider";
 
 const Login = () => {
-    console.log("Login Page Loaded");
+    const [logError, setLogError] = useState(null);
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setLogError(null);
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/xusers/user", {
+                method: "POST",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify({ email, password })
+            })
+
+            if (!response.ok) {
+                throw new Error("Invalid Credentials");
+            }
+
+            const userData = await response.json();
+            login(userData);
+            navigate("/");
+
+        } catch (error) {
+            setLogError(error.message);
+        }
+    }
+
     return (
-        <div>
-            <h1 className="text-3xl">
-                Login Page
-            </h1>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content grid grid-cols-1 gap-14 lg:grid-cols-2">
+                <div className="">
+                    <img src={authImg} alt="" />
+                </div>
+                <div className="card w-full shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-3xl font-bold mb-2">Login!</h1>
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" name="email" required className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" name="password" required className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                                <span className="label-text-alt p-1 text-error">{logError}</span>
+                            </div>
+                            <div className="form-control mt-1 p-1">
+                                <small>New to Lego Wonderland!? <Link className="text-[#799eb0] font-bold" to="/register">Register</Link></small>
+                            </div>
+                            <div className="form-control mt-5">
+                                <input className="btn bg-[#799eb0]" value="Login" type="submit" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
